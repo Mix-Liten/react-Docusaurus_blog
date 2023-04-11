487f851b09471657fcf3fc16b4f72b8633564582
Mix-Liten <mixliten@gmail.com>
2023-04-11 12:57:02 +0800

    update config
    footer link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,8 +90,8 @@ const config = {
             title: "Docs",
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
+                label: "Markdown Features",
+                to: "/npm/npm-docusaurus",
               },
             ],
           },
@@ -99,34 +99,22 @@ const config = {
             title: "Community",
             items: [
               {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/Mix-Liten",
+              },
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/in/ian-8b6913147",
               },
             ],
           },
+          // {
+          //   title: "More",
+          //   items: [
+          //   ],
+          // },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Mix-Liten, Inc. Built with Docusaurus.`,
       },
     }),
 };
